a5aabecb56f0d136257e52e59801b315cbc437e0
Popeskul Pavlo <peterjj91@gmail.com>
2020-03-29 19:20:39 +0300

    Main slide + slider mobile

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -2,6 +2,31 @@ $(document).ready(function() {
   $(".bootstrap-select").selectpicker();
 });
 
+$(".slider-cards").slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // centerMode: true,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 1250,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
 // $(document).ready(function() {
 //   $('#dropdown-service').on('click', function(e) {
 //     $('#navbarSupportedContent').removeClass('collapse show');
