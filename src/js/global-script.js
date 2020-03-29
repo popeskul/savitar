81170cfc92f6b36090ac0b8f418710dcecc9883f
Popeskul Pavlo <peterjj91@gmail.com>
2020-03-29 22:29:57 +0300

    Partners mobile

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -27,6 +27,16 @@ $(".slider-cards").slick({
   ]
 });
 
+$(".slider-partners").slick({
+  dots: true,
+  infinite: false,
+  arrows: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  vertical: true
+});
+
 // $(document).ready(function() {
 //   $('#dropdown-service').on('click', function(e) {
 //     $('#navbarSupportedContent').removeClass('collapse show');
